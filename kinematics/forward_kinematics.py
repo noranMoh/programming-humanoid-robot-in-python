59ad9fb8f0761173f28780a1a372521a2fbda387
noranabdelsalam <noran130897@gmail.com>
2020-07-03 16:31:46 +0200

    Forward Kinematics

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -51,32 +51,33 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
                        }
 
     def think(self, perception):
+        """"
         target_joints = {}
         target_joints["HeadYaw"] = -0.018500490114092827
         target_joints["HeadPitch"] = 0.07670722156763077
-        target_joints["LShoulderPitch"] = 1.5323816537857056
-        target_joints["LShoulderRoll"] = 0.2618168294429779
-        target_joints["LElbowYaw"] = 0.0
-        target_joints["LElbowRoll"] = -0.1745329201221466
-        target_joints["LHipYawPitch"] = -0.0
-        target_joints["LHipRoll"] = -0.0
-        target_joints["LHipPitch"] = -0.7175921201705933
-        target_joints["LKneePitch"] = 1.3576916456222534
-        target_joints["LAnklePitch"] = -0.6400995254516602
-        target_joints["LAnkleRoll"] = -0.0
-        target_joints["RHipYawPitch"] = -0.0
-        target_joints["RHipRoll"] = 0.0
-        target_joints["RHipPitch"] = -0.7175921201705933
-        target_joints["RKneePitch"] = 1.3576916456222534
-        target_joints["RAnklePitch"] = -0.6400995254516602
-        target_joints["RAnkleRoll"] = 0.0
-        target_joints["RShoulderPitch"] = 1.5323816537857056
-        target_joints["RShoulderRoll"] = -0.2618168294429779
-        target_joints["RElbowYaw"] = -0.0
-        target_joints["RElbowRoll"] = 0.1745329201221466
-
-
+        target_joints["LShoulderPitch"] = 1.4679789543151855
+        target_joints["LShoulderRoll"] = -0.08894197642803192
+        target_joints["LElbowYaw"] = -0.6841865181922913
+        target_joints["LElbowRoll"] = -1.2118170261383057
+        target_joints["LHipYawPitch"] = -0.2637890577316284
+        target_joints["LHipRoll"] = -0.07878416031599045
+        target_joints["LHipPitch"] = -0.6887767314910889
+        target_joints["LKneePitch"] = 2.0018577575683594
+        target_joints["LAnklePitch"] = -1.1412882804870605
+        target_joints["LAnkleRoll"] = 0.0783652812242508
+        target_joints["RHipYawPitch"] = -0.2722539007663727
+        target_joints["RHipRoll"] = 0.0711570754647255
+        target_joints["RHipPitch"] = -0.6965259909629822
+        target_joints["RKneePitch"] = 2.018787384033203
+        target_joints["RAnklePitch"] = -1.1493691205978394
+        target_joints["RAnkleRoll"] = - 0.09470156580209732
+        target_joints["RShoulderPitch"] = 1.5769747495651245
+        target_joints["RShoulderRoll"] = - 0.030752701684832573
+        target_joints["RElbowYaw"] = 0.9004851579666138
+        target_joints["RElbowRoll"] = 1.2426046133041382
         self.target_joints.update(target_joints)
+        """
+
         self.forward_kinematics(perception.joint)
         return super(ForwardKinematicsAgent, self).think(perception)
 
@@ -90,63 +91,55 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
         '''
 
         x = {'HeadYaw': 0.0, 'HeadPitch': 0.0, 'LShoulderPitch': 0.0, 'LShoulderRoll': 0.0, 'LElbowYaw': 105.0, 'LElbowRoll': 0.0, 'LWristYaw': 55.0,
-        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0, 'RElbowYaw': 105.0, 'RElbowRoll': 0.0, 'RWristYaw': 55.0, 'LHipYawPitch': 0.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':0.0, 'LAnklePitch':0.0,'LAnkleRoll':0.0,
+        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0, 'RElbowYaw': 105.0, 'RElbowRoll': 0.0, 'LHipYawPitch': 0.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':0.0, 'LAnklePitch':0.0,'LAnkleRoll':0.0,
         'RHipYawPitch': 0.0, 'RHipRoll': 0.0, 'RHipPitch': 0.0, 'RKneePitch': 0.0, 'RAnklePitch': 0.0, 'RAnkleRoll': 0.0}
 
-        y =  {'HeadYaw' : 0.0, 'HeadPitch' : 0.0, 'LShoulderPitch': 98.0, 'LShoulderRoll': 0.0 ,'LElbowYaw': 15.0,'LElbowRoll': 0.0, 'LWristYaw': 0.0,
-        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0 ,'RElbowYaw': 15.0,'RElbowRoll': 0.0, 'RWristYaw': 0.0,'LHipYawPitch': 50.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':0.0, 'LAnklePitch':0.0,'LAnkleRoll':0.0,
-        'RHipYawPitch':50.0, 'RHipRoll':0.0, 'RHipPitch':0.0, 'RKneePitch': 0.0, 'RAnklePitch':0.0,'RAnkleRoll' : 0.0}
+        y =  {'HeadYaw' : 0.0, 'HeadPitch' : 0.0, 'LShoulderPitch': 0.0, 'LShoulderRoll': 0.0 ,'LElbowYaw': 15.0,'LElbowRoll': 0.0, 'LWristYaw': 0.0,
+        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0 ,'RElbowYaw': -15.0,'RElbowRoll': 0.0,'LHipYawPitch': 0.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':0.0, 'LAnklePitch':0.0,'LAnkleRoll':0.0,
+        'RHipYawPitch': 0.0, 'RHipRoll':0.0, 'RHipPitch':0.0, 'RKneePitch': 0.0, 'RAnklePitch':0.0,'RAnkleRoll' : 0.0}
 
-        z =  {'HeadYaw' : 0.0, 'HeadPitch' : 0.0, 'LShoulderPitch': 0.0, 'LShoulderRoll': 0.0,'LElbowYaw': 105,'LElbowRoll': 0.0,
-        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0 ,'RElbowYaw': 105,'RElbowRoll': 0.0,'LHipYawPitch': 0.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':0.0, 'LAnklePitch':0.0,'LAnkleRoll':0.0,
-        'RHipYawPitch': 0.0, 'RHipRoll':0.0, 'RHipPitch':0.0, 'RKneePitch': 0.0, 'RAnklePitch': 0.0,'RAnkleRoll' : 0.0}
-
-
-        alpha = {'HeadYaw' : 0.0, 'HeadPitch' : -math.pi/2, 'LShoulderPitch': -math.pi/2, 'LShoulderRoll': math.pi/2 ,'LElbowYaw': math.pi/2,'LElbowRoll': -math.pi/2,
-        'RShoulderPitch': - math.pi/2, 'RShoulderRoll': math.pi/2,'RElbowYaw': math.pi/2,'RElbowRoll': -math.pi/2,'LHipYawPitch': -3/4 * math.pi, 'LHipRoll': -math.pi/2, 'LHipPitch':math.pi/2, 'LKneePitch': 0.0, 'LAnklePitch':0.0,'LAnkleRoll':-math.pi/2,
-        'RHipYawPitch': - math.pi/4, 'RHipRoll': -math.pi/2, 'RHipPitch': math.pi/2, 'RKneePitch': 0.0, 'RAnklePitch': 0.0,'RAnkleRoll' : -math.pi/2}
-
-        a = {'HeadYaw': 0.0, 'HeadPitch': 0.0, 'LShoulderPitch': 0.0,  'LShoulderRoll': 0.0, 'LElbowYaw': 15, 'LElbowRoll': 0.0,
-                 'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0, 'RElbowYaw': -15, 'RElbowRoll': 0.0, 'LHipYawPitch': 0.0, 'LHipRoll': 0.0, 'LHipPitch': 0.0, 'LKneePitch': -100.0, 'LAnklePitch': -102.90,
-                 'LAnkleRoll': 0.0,'RHipYawPitch': 0.0, 'RHipRoll': 0.0, 'RHipPitch': 0.0, 'RKneePitch': -100.0, 'RAnklePitch': -102.90,'RAnkleRoll': 0.0}
-
-        theta = {'HeadYaw': 0.0, 'HeadPitch': -math.pi/2, 'LShoulderPitch':0.0,  'LShoulderRoll': math.pi/2, 'LElbowYaw': 0.0, 'LElbowRoll': 0.0,
-                 'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0, 'RElbowYaw': 0.0, 'RElbowRoll': 0.0, 'LHipYawPitch': -math.pi/2, 'LHipRoll': math.pi/4, 'LHipPitch': 0.0, 'LKneePitch': 0.0, 'LAnklePitch': 0.0,
-                 'LAnkleRoll': 0.0,'RHipYawPitch': -math.pi/2, 'RHipRoll': -math.pi/4, 'RHipPitch': 0.0, 'RKneePitch': 0.0, 'RAnklePitch': 0.0,'RAnkleRoll': 0.0}
+        z =  {'HeadYaw' : 0.0, 'HeadPitch' : 0.0, 'LShoulderPitch': 0.0, 'LShoulderRoll': 0.0,'LElbowYaw': 0.0,'LElbowRoll': 0.0,
+        'RShoulderPitch': 0.0, 'RShoulderRoll': 0.0,'RElbowYaw': 0.0,'RElbowRoll': 0.0,'LHipYawPitch': 0.0, 'LHipRoll':0.0, 'LHipPitch':0.0, 'LKneePitch':-100.00, 'LAnklePitch': -102.9,'LAnkleRoll':0.0,
+        'RHipYawPitch': 0.0, 'RHipRoll':0.0, 'RHipPitch':0.0, 'RKneePitch': -100.00, 'RAnklePitch': -102.9,'RAnkleRoll' : 0.0}
 
         T = identity(4)
-        T1 = identity(4)
-        #joint_angle = joint_angle + alpha[joint_name]
-        #if joint_name == "HeadPitch":
-        #    joint_angle = joint_angle - math.pi/2
-        # YOUR CODE HERE
 
-        joint_angle = joint_angle + theta[joint_name]
-        T = [[math.cos(joint_angle), -math.sin(joint_angle) * math.cos(alpha[joint_name]), math.sin(joint_angle) * math.sin(alpha[joint_name]), a[joint_name] * (10 ** -3) * math.cos(joint_angle)],
-             [math.sin(joint_angle), math.cos(joint_angle) * math.cos(alpha[joint_name]), -math.cos(joint_angle) * math.sin(alpha[joint_name]), a[joint_name] * (10 ** -3)*math.sin(joint_angle)],
-             [0, math.sin(alpha[joint_name]),  math.cos(alpha[joint_name]), z[joint_name] * (10 ** -3)],
-             [0, 0, 0, 1]]
-        """"
-        if joint_name.find('Pitch') != -1:
-            T = [[math.cos(joint_angle), 0, math.sin(joint_angle), x[joint_name]* (10**-3)],
-                 [0, 1, 0, y[joint_name] * (10**-3)],
-                 [-math.sin(joint_angle), 0, math.cos(joint_angle), z[joint_name]* (10**-3)],
-                 [0, 0, 0, 1]]
-        elif joint_name.find('Yaw') != -1:
-            T = [[math.cos(joint_angle), -math.sin(joint_angle), 0, x[joint_name]*(10**-3)],
+        if joint_name.find("LHipYawPitch")!= -1:
+           T = matrix([[math.cos(joint_angle), -math.sin(joint_angle), 0, x[joint_name]*(10**-3)],
                  [math.sin(joint_angle), math.cos(joint_angle), 0, y[joint_name]*(10**-3)],
                  [0, 0, 1, z[joint_name] * (10**-3)],
-                 [0, 0, 0, 1]]
-        else:
-            T = [[1, 0, 0, x[joint_name]*(10**-3)],
+                 [0, 0, 0, 1]])
+           T =  T * matrix([[math.cos(joint_angle), 0, math.sin(joint_angle), x[joint_name]* (10**-3)],
+                 [0, 1, 0, y[joint_name] * (10**-3)],
+                 [-math.sin(joint_angle), 0, math.cos(joint_angle), z[joint_name]* (10**-3)],
+                 [0, 0, 0, 1]])
+        elif joint_name.find("RHipYawPitch") != -1:
+            T = matrix([[math.cos(joint_angle), -math.sin(joint_angle), 0, x[joint_name] * (10 ** -3)],
+                        [math.sin(joint_angle), math.cos(joint_angle), 0, y[joint_name] * (10 ** -3)],
+                        [0, 0, 1, z[joint_name] * (10 ** -3)],
+                        [0, 0, 0, 1]])
+            T = T * matrix([[math.cos(joint_angle), 0, math.sin(joint_angle), x[joint_name] * (10 ** -3)],
+                            [0, 1, 0, y[joint_name] * (10 ** -3)],
+                            [-math.sin(joint_angle), 0, math.cos(joint_angle), z[joint_name] * (10 ** -3)],
+                            [0, 0, 0, 1]])
+
+        elif joint_name.find("Pitch") != -1: #pitch
+            T = matrix([[math.cos(joint_angle), 0, math.sin(joint_angle), x[joint_name]* (10**-3)],
+                 [0, 1, 0, y[joint_name] * (10**-3)],
+                 [-math.sin(joint_angle), 0, math.cos(joint_angle), z[joint_name]* (10**-3)],
+                 [0, 0, 0, 1]])
+        elif joint_name.find("ShoulderRoll") != -1 or joint_name.find("ElbowRoll") != -1 or joint_name == "HeadYaw": #Yaw
+            T = matrix([[math.cos(joint_angle), -math.sin(joint_angle), 0, x[joint_name]*(10**-3)],
+                 [math.sin(joint_angle), math.cos(joint_angle), 0, y[joint_name]*(10**-3)],
+                 [0, 0, 1, z[joint_name] * (10**-3)],
+                 [0, 0, 0, 1]])
+        else: #roll    # HipRoll ,
+            T = matrix([[1, 0, 0, x[joint_name]*(10**-3)],
                  [0, math.cos(joint_angle), -math.sin(joint_angle), y[joint_name]*(10**-3)],
                  [0, math.sin(joint_angle), math.cos(joint_angle), z[joint_name]*(10**-3)],
-                 [0, 0, 0, 1]]
+                 [0, 0, 0, 1]])
         #print (T)
-      """""
-        if joint_name.find("Head") != -1:
-           print (joint_name)
-           print (T)
+
         return T
 
     def forward_kinematics(self, joints):
@@ -159,57 +152,48 @@ class ForwardKinematicsAgent(AngleInterpolationAgent):
             for joint in chain_joints:
                 T1 = identity(4)
                 if joint == "HeadYaw":
-                    T1 = [[1, 0, 0, 0],
+                    T1 = matrix([[1, 0.0, 0, 0],
                           [0, 1, 0, 0],
                           [0, 0, 1, 0.1265],
-                          [0, 0, 0, 1]]
+                          [0, 0, 0, 1]])
                 elif joint == "RShoulderPitch":
-                    T1 = [[1, 0, 0, 0],
+                    T1 = matrix([[1, 0, 0, 0],
                           [0, 1, 0, -0.098],
                           [0, 0, 1, 0.100],
-                          [0, 0, 0, 1]]
+                          [0, 0, 0, 1]])
                 elif joint == "LShoulderPitch":
-                    T1 = [[1, 0, 0, 0],
+                    T1 = numpy.matrix([[1, 0, 0, 0],
                           [0, 1, 0, 0.098],
                           [0, 0, 1, 0.100],
-                          [0, 0, 0, 1]]
+                          [0, 0, 0, 1]])
                 elif joint == "LHipYawPitch":
-                    T1 = [[1, 0, 0, 0],
+                    T1 = matrix([[1, 0, 0, 0],
                           [0, 1, 0, 0.050],
                           [0, 0, 1, -0.085],
-                          [0, 0, 0, 1]]
+                          [0, 0, 0, 1]])
                 elif joint == "RHipYawPitch":
-                    T1 = [[1, 0, 0, 0],
+                    T1 = matrix([[1, 0, 0, 0],
                           [0, 1, 0, -0.050],
                           [0, 0, 1, -0.085],
-                          [0, 0, 0, 1]]
+                          [0, 0, 0, 1]])
+
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
-                # YOUR CODE HERE
-                T = T * Tl *T1
+                T = T1 * T * Tl
                 self.transforms[joint] = T
 
-
-
-        ry = [[math.cos(math.pi/2), 0,math.sin(math.pi/2), 0],
-                 [0, 1, 0, 0],
-                 [-math.sin(math.pi/2), 0, math.cos(math.pi/2),0],
-                 [0, 0, 0, 1]]
-        m =  self.transforms["RElbowRoll"]
+        """
+        m = self.transforms["RAnkleRoll"]
         a = numpy.array(m)
 
-        print "x" + repr(a[0][3])
-        print "y" + repr(a[1][3])
-        print "z" + repr(a[2][3])
+        print "x: " + repr(a[0][3])
+        print "y: " + repr(a[1][3])
+        print "z: " + repr(a[2][3])
 
-        print "Roll" + repr(math.atan2(a[2][1], a[2][2]))
-        print "pitch" + repr(math.atan2(-a[2][0], math.sqrt(a[2][1] ** 2 + a[2][2] ** 2)))
-        print "yaw" + repr(math.atan2(a[1][0], a[0][0]))
-        """
-        print "Roll" + repr(math.atan2(a[2][1], a[1][1]))
-        print "pitch" + repr(math.atan2(a[0][2],a[0][0]))
-        print "yaw" + repr(math.atan2(a[1][0], a[0][0]))
-    """
+        print "Roll: " + repr(math.atan2(a[2][1], a[2][2]))
+        print "pitch: " + repr(math.atan2(-a[2][0], math.sqrt(a[2][1] ** 2 + a[2][2] ** 2)))
+        print "yaw: " + repr(math.atan2(a[1][0], a[0][0]))
+      """
 if __name__ == '__main__':
     agent = ForwardKinematicsAgent()
     agent.run()
